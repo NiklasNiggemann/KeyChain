2adc47f3b9d182f93d575c2da59c2840117f00c2
NiklasNiggemann <niklasniggemann@yahoo.de>
2022-03-02 11:19:36 +0100

    setup

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 const text = document.getElementById('text');
-const key = document.getElementById('key');
-const mode = document.getElementById('mode');
+const keyLabel = document.getElementById('key');
+const modeLabel = document.getElementById('mode');
 
-let keys;
-function resetKeys() {
-    keys = [
-        'C ', 'C# ', 'D ', 'D# ', 'E ', 'F ', 'F# ', 'G ', 'G# ', 'A ', 'A# ', 'B ',
-        'C ', 'C# ', 'D ', 'D# ', 'E ', 'F ', 'F# ', 'G ', 'G# ', 'A ', 'A# ', 'B '
-    ];
+let keys = [
+    /*0    1      2     3      4     5     6      7     8      9     10     11*/
+    'C ', 'C# ', 'D ', 'D# ', 'E ', 'F ', 'F# ', 'G ', 'G# ', 'A ', 'A# ', 'B ',
+    /*12   13     14    15     16    17    18     19    20     21    22     23*/
+    'C ', 'C# ', 'D ', 'D# ', 'E ', 'F ', 'F# ', 'G ', 'G# ', 'A ', 'A# ', 'B '
+];
+
+let modes = [
+    /*0       1         2           3*/
+    'Major', 'Dorian', 'Phrygian', 'Lydian',
+    /*4            5        6*/
+    'Mixolydian', 'Minor', 'Locrian'
+]
+
+var mode;
+var choosenMode = 0;
+
+function declareMode(thisMode) {
+    choosenMode = thisMode;
+    mode = modes[thisMode];
+    modeLabel.innerHTML = mode;
 }
+
+function declareRoot(root, choosenMode) {
+
+    keyLabel.innerHTML = keys[root];
+
+    switch (choosenMode) {
+        case 0:
+            text.innerHTML = keys[root] + keys[root + 2] + keys[root + 4] +
+                keys[root + 5] + keys[root + 7] + keys[root + 9] + keys[root + 11];
+        case 1:
+            text.innerHTML = keys[root] + keys[root + 3] + keys[root + 5] +
+                keys[root + 6] + keys[root + 7] + keys[root + 9] + keys[root + 11];
+        default:
+            text.innerHTML = keys[root] + keys[root + 2] + keys[root + 4] +
+                keys[root + 5] + keys[root + 7] + keys[root + 9] + keys[root + 11];
+    }
+
+}
+
+
+/* 
+
 let keyInterval;
 let whatMode = 'major';
 let root;
@@ -28,37 +65,37 @@ let B;
 
 function itIsMajor() {
     whatMode = 'major';
-    mode.innerHTML = 'major';
+    mode.innerHTML = 'Major';
 }
 
 function itIsDorian() {
     whatMode = 'dorian';
-    mode.innerHTML = 'dorian';
+    mode.innerHTML = 'Dorian';
 }
 
 function itIsPhrygian() {
     whatMode = 'phrygian';
-    mode.innerHTML = 'phrygian';
+    mode.innerHTML = 'Phrygian';
 }
 
 function itIsLydian() {
     whatMode = 'lydian';
-    mode.innerHTML = 'lydian';
+    mode.innerHTML = 'Lydian';
 }
 
 function itIsMixolydian() {
     whatMode = 'mixolydian';
-    mode.innerHTML = 'mixolydian';
+    mode.innerHTML = 'Mixolydian';
 }
 
 function itIsMinor() {
     whatMode = 'minor';
-    mode.innerHTML = 'minor';
+    mode.innerHTML = 'Minor';
 }
 
 function itIsLocrian() {
     whatMode = 'locrian';
-    mode.innerHTML = 'locrian';
+    mode.innerHTML = 'Locrian';
 }
 
 function showKey(root) {
@@ -70,42 +107,42 @@ function showKey(root) {
             case 'major':
                 text.innerHTML = keys[root] + keys[root + 2] + keys[root + 4] + keys[root + 5] + keys[root + 7] + keys[root + 9] + keys[root + 11];
                 if (C) {
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (Cs) {
                     keys[5] = 'E# ';
                     keys[12] = 'B# ';
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (D) {
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (Eb) {
                     keys[3] = 'Eb ';
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (F) {
                     keys[10] = 'Bb ';
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (E) {
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (Fs) {
                     keys[17] = 'E# ';
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (G) {
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (Ab) {
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
                     keys[13] = 'Db ';
                     keys[15] = 'Eb ';
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (A) {
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (Bb) {
                     keys[10] = 'Bb ';
                     keys[15] = 'Eb ';
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 } else if (B) {
-                    mode.innerHTML = 'major';
+                    mode.innerHTML = 'Major';
                 }
                 break;
             case 'dorian':
@@ -133,7 +170,7 @@ function showKey(root) {
                     keys[15] = 'Eb ';
                     keys[13] = 'Db ';
                     keys[20] = 'Ab ';
-                } 
+                }
                 break;
             case 'phrygian':
                 text.innerHTML = keys[root] + keys[root + 1] + keys[root + 3] + keys[root + 5] + keys[root + 7] + keys[root + 8] + keys[root + 10];
@@ -148,44 +185,44 @@ function showKey(root) {
                 } else if (D) {
                     keys[3] = 'Eb ';
                     keys[10] = 'Bb ';
-                } else if (Eb) {           
+                } else if (Eb) {
                     keys[3] = 'Eb ';
                     keys[4] = 'Fb ';
                     keys[6] = 'Gb ';
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
                     keys[11] = 'Cb ';
-                    keys[13] = 'Db '; 
+                    keys[13] = 'Db ';
                 } else if (F) {
                     keys[6] = 'Gb ';
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
-                    keys[13] = 'Db '; 
+                    keys[13] = 'Db ';
                     keys[15] = 'Eb ';
                 } else if (Fs) {
                     keys[17] = 'E# ';
-                } else if (G) {            
+                } else if (G) {
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
                     keys[15] = 'Eb ';
-                } else if (Ab) {          
+                } else if (Ab) {
                     keys[8] = 'G# ';
                     keys[13] = 'C# ';
                     keys[15] = 'D# ';
-                } else if (A) {            
+                } else if (A) {
                     keys[10] = 'Bb ';
-                } else if (Bb) {            
+                } else if (Bb) {
                     keys[10] = 'Bb ';
                     keys[11] = 'Cb ';
                     keys[13] = 'Db ';
                     keys[15] = 'Eb ';
                     keys[18] = 'Gb ';
                     keys[20] = 'Ab ';
-                } 
+                }
                 break;
             case 'lydian':
                 text.innerHTML = keys[root] + keys[root + 2] + keys[root + 4] + keys[root + 6] + keys[root + 7] + keys[root + 9] + keys[root + 11];
-                if (Cs) {            
+                if (Cs) {
                     keys[1] = 'Db ';
                     keys[3] = 'Eb ';
                     keys[8] = 'Ab ';
@@ -201,108 +238,108 @@ function showKey(root) {
                 } else if (Bb) {
                     keys[10] = 'Bb ';
                     keys[15] = 'Eb ';
-                } else if (B) {       
+                } else if (B) {
                     keys[17] = 'E# ';
                 }
                 break;
             case 'mixolydian':
                 text.innerHTML = keys[root] + keys[root + 2] + keys[root + 4] + keys[root + 5] + keys[root + 7] + keys[root + 9] + keys[root + 10];
-                if (C) {            
+                if (C) {
                     keys[10] = 'Bb ';
                 } else if (Cs) {
                     keys[5] = 'E# ';
                     keys[12] = 'B# ';
-                } else if (Eb) {            
+                } else if (Eb) {
                     keys[3] = 'Eb ';
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
                     keys[13] = 'Db ';
-                } else if (F) {            
+                } else if (F) {
                     keys[10] = 'Bb ';
                     keys[15] = 'Eb ';
-                } else if (Ab) {            
+                } else if (Ab) {
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
                     keys[13] = 'Db ';
                     keys[15] = 'Eb ';
                     keys[18] = 'Gb ';
-                } else if (Bb) {            
+                } else if (Bb) {
                     keys[10] = 'Bb ';
                     keys[15] = 'Eb ';
                     keys[20] = 'Ab ';
-                } 
+                }
                 break;
             case 'minor':
                 text.innerHTML = keys[root] + keys[root + 2] + keys[root + 3] + keys[root + 5] + keys[root + 7] + keys[root + 8] + keys[root + 10];
-                if (C) {            
+                if (C) {
                     keys[3] = 'Eb ';
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
-                } else if (D) {            
+                } else if (D) {
                     keys[10] = 'Bb ';
-                } else if (Eb) {            
-                    keys[3] = 'Eb '; 
+                } else if (Eb) {
+                    keys[3] = 'Eb ';
                     keys[6] = 'Gb ';
-                    keys[8] = 'Ab '; 
-                    keys[10] = 'Bb '; 
+                    keys[8] = 'Ab ';
+                    keys[10] = 'Bb ';
                     keys[11] = 'Cb ';
                     keys[13] = 'Db ';
-                } else if (F) {            
-                    keys[10] = 'Bb '; 
+                } else if (F) {
+                    keys[10] = 'Bb ';
                     keys[8] = 'Ab ';
                     keys[13] = 'Db ';
                     keys[15] = 'Eb ';
                 } else if (Fs) {
-                    keys[17] = 'E# '; 
-                } else if (G) {            
+                    keys[17] = 'E# ';
+                } else if (G) {
                     keys[10] = 'Bb ';
                     keys[15] = 'Eb ';
-                } else if (Ab) {            
-                    keys[8] = 'Ab '; 
-                    keys[10] = 'Bb '; 
+                } else if (Ab) {
+                    keys[8] = 'Ab ';
+                    keys[10] = 'Bb ';
                     keys[11] = 'Cb ';
-                    keys[13] = 'Db '; 
-                    keys[15] = 'Eb '; 
+                    keys[13] = 'Db ';
+                    keys[15] = 'Eb ';
                     keys[16] = 'Fb ';
                     keys[18] = 'Gb ';
-                } else if (Bb) {            
+                } else if (Bb) {
                     keys[10] = 'Bb ';
                     keys[13] = 'Db ';
                     keys[15] = 'Eb ';
                     keys[18] = 'Gb ';
                     keys[20] = 'Ab ';
-                } 
+                }
                 break;
             case 'locrian':
                 text.innerHTML = keys[root] + keys[root + 1] + keys[root + 3] + keys[root + 5] + keys[root + 6] + keys[root + 8] + keys[root + 10];
-                if (C) {            
+                if (C) {
                     keys[1] = 'Db ';
                     keys[3] = 'Eb ';
                     keys[6] = 'Gb ';
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
-                } else if (D) {          
+                } else if (D) {
                     keys[3] = 'Eb ';
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
-                } else if (F) {           
+                } else if (F) {
                     keys[6] = 'Gb ';
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
                     keys[11] = 'Cb ';
                     keys[13] = 'Db ';
-                    keys[15] = 'Eb '; 
-                } else if (E) {            
+                    keys[15] = 'Eb ';
+                } else if (E) {
                     keys[10] = 'Bb ';
                 } else if (G) {
                     keys[8] = 'Ab ';
                     keys[10] = 'Bb ';
                     keys[13] = 'Db ';
                     keys[15] = 'Eb ';
-                } else if (A) {            
+                } else if (A) {
                     keys[10] = 'Bb ';
                     keys[15] = 'Eb ';
-                } else if (Bb) {            
+                } else if (Bb) {
                     keys[10] = 'Bb ';
                     keys[11] = 'Cb ';
                     keys[13] = 'Db ';
@@ -310,7 +347,7 @@ function showKey(root) {
                     keys[16] = 'Fb ';
                     keys[18] = 'Gb ';
                     keys[20] = 'Ab ';
-                } 
+                }
                 break;
         }
     }
@@ -545,3 +582,4 @@ function showB() {
     showKey(11);
 }
 
+*/
